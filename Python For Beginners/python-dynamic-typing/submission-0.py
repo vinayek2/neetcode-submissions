788a40b320a8427b1250e91735f14c69ad9d37be
vinayek2 <114267366+vinayek2@users.noreply.github.com>
2026-07-19 14:22:42 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10 
+print(type(variable))
+variable = 10.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hell"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
